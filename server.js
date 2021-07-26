a3a3e5ea63fa6d4f7b40c1725035a9cb189b5668
annejenkins-cgi <73593442+annejenkins-cgi@users.noreply.github.com>
2021-07-26 12:54:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const http = require('http')
 const fs = require('fs')
-const twilio_voice = require('teneo-web-chat.js');
 
-const twilio_voice_instance = new twilio_voice();
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html' })
   fs.createReadStream('collections_demo.html').pipe(res)
