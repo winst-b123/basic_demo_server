ea5f37b7375006f9960505b833b9606fe0ba81ef
annejenkins-cgi <73593442+annejenkins-cgi@users.noreply.github.com>
2021-07-26 12:58:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const http = require('http')
 const fs = require('fs')
 
+const twilio_voice = require(path.resolve('/', 'twilio_voice.js'));
+const twilio_voice_instance = new twilio_voice();
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html' })
   fs.createReadStream('collections_demo.html').pipe(res)
