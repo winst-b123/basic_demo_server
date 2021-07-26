6aefa3c94b0be7f2b086e8dbe9f8de111258676b
annejenkins-cgi <73593442+annejenkins-cgi@users.noreply.github.com>
2021-07-26 13:02:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const fs = require('fs')
 
-const twilio_voice = require(path.resolve('/app/', 'twilio_voice.js'));
+const twilio_voice = require(path.resolve('/app', 'twilio_voice.js'));
 const twilio_voice_instance = new twilio_voice();
 
 const server = http.createServer((req, res) => {
