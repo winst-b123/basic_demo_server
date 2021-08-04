79475f87d492397461078f584877363c286fedd3
annejenkins-cgi <73593442+annejenkins-cgi@users.noreply.github.com>
2021-08-04 09:54:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const fs = require('fs')
 const path = require('path');
 const app = express();
+const router = express.Router();
+
+// Tell express to use this router with /api before.
+app.use("/", router);
 app.use("/images", express.static(path.join(__dirname, 'images')));
 
 //const collectionspage = require(path.resolve('/images', 'collectionspage.jpg'));
